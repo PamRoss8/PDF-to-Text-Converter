501f21de9ecf8bc7aedfc032b36bea0d60d2635b
PamRoss8 <stepnzorskij@gmail.com>
2025-02-09 23:44:54 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# PDF-to-Text-Converter
+# PDF to Text Converter
+
+This is a Python project for pdf to text converter. More details will be added soon.
---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+PDF to Text Converter
+This is the main script for the pdf to text converter project.
+"""
+
+def main():
+    print("Welcome to PDF to Text Converter!")
+
+if __name__ == "__main__":
+    main()
